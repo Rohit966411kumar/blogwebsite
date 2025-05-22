4bd89b3e4ae73bb330a497ec46d5eed2c98f1631
Rohit966411kumar <160835536+Rohit966411kumar@users.noreply.github.com>
2025-05-22 18:18:58 +0000

    this is my first laravel project commit

---
+++ resources/views/front/layout/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+    @include('front.include.head')
+<body>
+    @include('front.include.header')
+
+    @yield('content')
+
+    @include('front.include.footer')
+@include('front.include.footerscripts')
+</body>
+</html>
